f3502408a4cfe6cf8cb6cedd332af590e3dd3d15
re4nightwing <dulan9595531@gmail.com>
2021-09-12 10:25:46 +0530

    round 01 edits done

--- js/main.js
+++ js/main.js
@@ -75,7 +75,7 @@ document.querySelectorAll(".dropdown-toggle").forEach((item) => {
   });
 });
 // Set the date we're counting down to
-var countDownDate = new Date("Sept 9, 2021 23:59:59").getTime();
+var countDownDate = new Date("Sept 13, 2021 12:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {
